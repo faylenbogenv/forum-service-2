734c3690eb9eae9b2fd33430a38d7312c41e156e
faylenbogenv <faylenbogenv@gmail.com>
2024-05-31 18:06:33 +0300

    Final commit

---
+++ src/main/java/telran/java52/security/CustomWebSecurity.java
@@ -0,0 +1,18 @@
+package telran.java52.security;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java52.post.dao.PostRepository;
+import telran.java52.post.model.Post;
+
+@Service("webSecurity")
+@RequiredArgsConstructor
+public class CustomWebSecurity {
+	final PostRepository postRepository;
+	
+	public boolean checkPostAuthor(String postId, String userName) {
+		Post post = postRepository.findById(postId).orElse(null);
+		return post != null && post.getAuthor().equalsIgnoreCase(userName);
+	}
+}
